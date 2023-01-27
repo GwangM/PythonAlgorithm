9f579864938239a182af891140fc962a49b008d6
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-01-28 00:34:16 +0900

    2023-01-28 commit

--- backjoon/class2/boj1966.py
+++ backjoon/class2/boj1966.py
@@ -1,15 +1,27 @@
-
+#프린터큐
 k=int(input())
 import sys
 input = sys.stdin.readline
 for _ in range(k):
   n,m=map(int, input().split())
+  idx=[i for i in range(n)]
   ary=list(map(int,input().split()))
   temp=ary[m]
-  print(ary)
+  origin=ary[:]
+  
   ary.sort(reverse=True)
-  print(ary)
-  print(ary.index(temp)+1)
+  cnt=1
+  for i in ary:
+    id=origin.index(i)
+    origin=origin[id:]+origin[:id]
+    idx=idx[id:]+idx[:id]
+    if idx[0]==m:
+      print(cnt)
+      break
+    else:
+      origin.pop(0)
+      idx.pop(0)
+      cnt+=1
   #가장 높은 수를 찾고 그 index 앞의 것들은 전부 뒤로 보내야
   #slicing [d:] + [:d] 뒤로 붙여준다. index를 저장해두고 바뀔 때마다 변경하는 방식으로 
   
