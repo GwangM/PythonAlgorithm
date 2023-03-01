1882d7feaa43ff07329c937c5dd991efe2fba11a
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-03-01 23:20:25 +0900

    create boj1676.py

--- backjoon/class3/boj7662.py
+++ backjoon/class3/boj7662.py
@@ -5,15 +5,33 @@ input=sys.stdin.readline
 t=int(input())
 for _ in range(t):
     k=int(input())
+    le=0
     arr=[]
+    reverse=[]
     for _ in range(k):
         o,n=input().split()
         n=int(n)
         if o=='I':
             heapq.heappush(arr,n)
-        elif len(arr)==0:
+            heapq.heappush(reverse,(-n,n))
+            le+=1
+        elif le==0:
             continue 
         elif n==1:
-            heapq.heappop()           
+            aaa,temp_=heapq.heappop(reverse)
+            for i in range(le-1,0,-1):
+                if arr[i]==temp_:
+                    arr.pop(i)
+                    break
+            le-=1
         else:
-            heapq.heappop()
+            temp_=heapq.heappop(arr)
+            for i in range(le-1,0,-1):
+                if reverse[i][1]==temp_:
+                    reverse.pop(i)
+                    break
+            le-=1
+    if le==0:
+        print("EMPTY")
+    else:
+        print(reverse[0][1],arr[0])
