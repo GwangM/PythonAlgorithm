0aa0924f24bdaa66f4c09d39c9605b18ec51cda8
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-03-24 23:17:07 +0900

    modify boj2606.py

--- backjoon/class3/boj2606.py
+++ backjoon/class3/boj2606.py
@@ -1,7 +1,28 @@
 import sys
+from collections import deque
 input=sys.stdin.readline
 t=int(input())
 n=int(input())
+arr=[[]for i in range(t+1)]
+
+def bfs(start,arr):
+    queue=deque([start])
+    visited=[start]
+    result=0
+    while queue:
+        temp=queue.popleft()
+        for i in arr[temp]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
+                result+=1
+
+    return result
+
+
 for i in range(n):
     a,b=map(int,input().split())
-    
+    arr[a].append(b)
+    arr[b].append(a)
+
+print(bfs(1,arr))
