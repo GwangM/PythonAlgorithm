269aa7465211e2a7e143e95803baa3080ff7e26f
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-03-26 00:34:21 +0900

    boj2805.py

--- backjoon/class2/boj2805.py
+++ backjoon/class2/boj2805.py
@@ -1,7 +1,25 @@
 n,m=map(int,input().split())
 tree=list(map(int,input().split()))
+tree.sort()
 start=0
-end=max(tree)
+end=tree[-1]
 #개수로 넣는 게 나을 것 같다.
+result=0
+le=len(tree)
 while(start<=end):
     mid=(start+end)//2
+    temp=0
+
+    for i in range(le-1,-1,-1):
+        if tree[i]<=mid:
+            break
+        else:
+            temp+=(tree[i]-mid)
+
+    if temp>=m and mid>result:
+        result=mid
+        start=mid+1
+    else:
+        end=mid-1
+
+print(result)
