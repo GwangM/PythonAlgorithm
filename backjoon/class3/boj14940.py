b9b0dc44aad9d3b8774061f97375927186f43a4b
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-06-20 23:45:23 +0900

    fixed boj14940.py

--- backjoon/class3/boj14940.py
+++ backjoon/class3/boj14940.py
@@ -13,25 +13,30 @@ for i in range(n):
         y=arr[i].index(2)
 
 distance=copy.deepcopy(arr)
-
+visited=copy.deepcopy(arr)
 def bfs(x,y,arr):
     queue=deque([(x,y,0)])
-    visited=[[x,y]]
+    visited[x][y]=-1
     while queue:
         tempX,tempY,dist=queue.popleft()
       
         distance[tempX][tempY]=dist
-        if tempX-1>=0 and arr[tempX-1][tempY]==1 and [tempX-1,tempY] not in visited:
-            visited.append([tempX-1,tempY])
+        if tempX-1>=0 and arr[tempX-1][tempY]==1 and visited[tempX-1][tempY]!=-1:
+            visited[tempX-1][tempY]=-1
             queue.append((tempX-1,tempY,dist+1))
-        if tempY-1>=0 and arr[tempX][tempY-1]==1 and [tempX,tempY-1] not in visited:
-            visited.append([tempX,tempY-1])
+        if tempY-1>=0 and arr[tempX][tempY-1]==1 and visited[tempX][tempY-1]!=-1:
+            visited[tempX][tempY-1]=-1
             queue.append((tempX,tempY-1,dist+1))
-        if tempX+1<=n-1 and arr[tempX+1][tempY]==1 and [tempX+1,tempY] not in visited:
-            visited.append([tempX+1,tempY])
+        if tempX+1<=n-1 and arr[tempX+1][tempY]==1 and visited[tempX+1][tempY]!=-1:
+            visited[tempX+1][tempY]=-1
             queue.append((tempX+1,tempY,dist+1))
-        if tempY+1<=m-1 and arr[tempX][tempY+1]==1 and [tempX,tempY+1] not in visited:
-            visited.append([tempX,tempY+1])
+        if tempY+1<=m-1 and arr[tempX][tempY+1]==1 and visited[tempX][tempY+1]!=-1:
+            visited[tempX][tempY+1]=-1
             queue.append((tempX,tempY+1,dist+1))
 bfs(x,y,arr)
-    
+for i in range(n):
+    for j in range(m):
+        if visited[i][j]!=-1 and arr[i][j]!=0:
+            distance[i][j]=-1
+for i in distance:
+    print(*i)
